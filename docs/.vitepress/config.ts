6030c2b347aa8cfc8a026c3797b73f38ea33177b
plajiw <pabloribeiro.cont@gmail.com>
2026-03-26 22:08:43 -0300

    a

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  ignoreDeadLinks: true,
   base: '/docs/',
   title: "Kerus Docs",
   description: "Documentação Técnica do Gerador de Fórmulas Kerus",
